b42bcc57a8b57ee8ef04ecbb33ea893f1e447c06
Pierre Tremblay <pierre.tremblay.1995@gmail.com>
2022-05-19 12:44:36 +0200

    création des cards météo

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
+import {ListComponent} from "./likes/list/list.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'likes', component: ListComponent}
 ];
 
 @NgModule({
