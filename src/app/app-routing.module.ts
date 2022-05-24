f91d37f945ffe79dda91d6893d00a769daeed724
AdrienPinheiro <adrienpnrpro@gmail.com>
2022-05-24 16:02:19 +0200

    commit 24/05/2022 Adrien Guard + logs fini

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: () => import('./visitor/visitor.module').then(m => m.VisitorModule) },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
-  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule)},
   { path: 'forum', loadChildren: () => import('./forum/forum.module').then(m => m.ForumModule) },
   { path: '**', redirectTo: '' }
 ];
