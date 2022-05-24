48b5b83a188f431067ddd77c7abb9aa7de51ed68
alinidhal <ali.nidhal@hotmail.com>
2022-05-24 16:42:33 +0200

    Add category, comment, post models

---
+++ src/app/shared/models/post.ts
@@ -0,0 +1,10 @@
+export interface Post {
+        id?: string,
+        id_user?: string,
+        title: string,
+        content: string,
+        image: string,
+        date: Date,
+        id_category?: string
+
+}
