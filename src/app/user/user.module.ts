3088c88333aaf355f677f962618951f132b5b260
Julien Picquet <68906097+OlderGlow@users.noreply.github.com>
2022-05-26 00:10:14 +0200

    add new dropdown and animation with angular animation

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,5 +1,5 @@
 import {LOCALE_ID, NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import {CommonModule, registerLocaleData} from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 import {ListComponent} from './likes/list/list.component';
 import {AddComponent} from './likes/add/add.component';
@@ -13,8 +13,8 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {SearchComponent} from './home/map/search/search.component';
 import {SearchAddressService} from "../shared/services/search-address.service";
 import {LeafletModule} from "@asymmetrik/ngx-leaflet";
-import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
+
 registerLocaleData(localeFr, 'fr');
 
 @NgModule({
@@ -34,10 +34,10 @@ registerLocaleData(localeFr, 'fr');
     HttpClientModule,
     FormsModule,
     LeafletModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   exports: [],
-  providers: [SearchAddressService,  {provide: LOCALE_ID, useValue: "fr-FR"}]
+  providers: [SearchAddressService, {provide: LOCALE_ID, useValue: "fr-FR"}]
 })
 export class UserModule {
 }
