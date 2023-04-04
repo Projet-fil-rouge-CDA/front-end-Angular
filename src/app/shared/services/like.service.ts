88d81482feb8952273ef3193e3313f24caf66450
Pierre <pierre.tremblay.1995@gmail.com>
2023-04-04 09:43:13 +0200

    Ajout des favoris au clic

--- src/app/shared/services/like.service.ts
+++ src/app/shared/services/like.service.ts
@@ -1,22 +1,35 @@
 import {Injectable} from '@angular/core';
 import {Weather} from "../models/weather.model";
 import {NominatimAddress} from "../models/nominatim-response.model";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class LikeService {
 
-  constructor() {
-  }
+    constructor(private httpClient: HttpClient) {
+    }
 
-  private weathers: Weather[];
+    private apiURL: string = environment.urlApi + "/user/station/like";
+    private weathers: Weather[];
+    private httpHeaders = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+        })
+    }
 
-  getWeathersLiked(): Weather[] {
-    return this.weathers.filter((weather: Weather) => {
-      return weather.liked;
-    });
-  }
+    getWeathersLiked(): Weather[] {
+        return this.weathers.filter((weather: Weather) => {
+            return weather.liked;
+        });
+    }
 
+    addFavoritesLikes(nomStation: string, pseudo: string, add: boolean) {
+        this.httpClient.post(this.apiURL + `?nomStation=${nomStation}&pseudo=${pseudo}&add=${add}`, {},
+            this.httpHeaders).subscribe();
+
+    }
 
 }
