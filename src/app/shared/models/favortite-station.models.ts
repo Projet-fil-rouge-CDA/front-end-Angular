a5e072240f85f258e0f2803ff863452717cf3bd7
Adrien PINHEIRO <adrienpnrpro@gmail.com>
2023-04-05 17:36:34 +0200

    Favorite good

---
+++ src/app/shared/models/favortite-station.models.ts
@@ -0,0 +1,12 @@
+export interface FavortiteStationModels{
+    nomVille:string,
+    nomStation:string,
+    codeStation:string,
+    temp:number,
+    skyInfo:string,
+    indiceAir:string,
+    indiceValue:number,
+    indiceUnite:string,
+    indiceDate:string,
+    clicked:boolean
+}
